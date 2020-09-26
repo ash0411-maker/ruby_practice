23bf86f4cc10b09c2b3004e7987b070a7fd0d365
ash <uetani.0411@gmail.com>
2020-09-26 13:50:34 +0900

    codewar is_isogram

--- pointcard.rb
+++ pointcard.rb
@@ -1,9 +1,14 @@
-# 自分の回答
-def createPhoneNumber(numbers)
-    return "(#{numbers[0]}#{numbers[1]}#{numbers[2]}) #{numbers[3]}#{numbers[4]}#{numbers[5]}-#{numbers[6]}#{numbers[7]}#{numbers[8]}#{numbers[9]}"
+def is_isogram(string)
+    string = string.downcase.chars
+    if string == string.uniq
+      return true
+    else
+      return false
+    end
 end
 
-# 理想の回答
-def createPhoneNumber(str)
-  "(#{str[0..2].join}) #{str[3..5].join}-#{str[6..10].join}"
+
+
+def is_isogram(string)
+  string.downcase.chars.uniq == string.downcase.chars
 end
