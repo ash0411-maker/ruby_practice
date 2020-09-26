52db827b8c8e265cbcec6ab840c5b77951b48f1e
ash <uetani.0411@gmail.com>
2020-09-26 13:55:20 +0900

    codewar is_isogram

--- pointcard.rb
+++ pointcard.rb
@@ -1,3 +1,4 @@
+#自分の回答
 def is_isogram(string)
     string = string.downcase.chars
     if string == string.uniq
@@ -8,7 +9,7 @@ def is_isogram(string)
 end
 
 
-
+#理想の回答方法
 def is_isogram(string)
   string.downcase.chars.uniq == string.downcase.chars
 end
