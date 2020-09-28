efc33dcd8cfbd76928f62999357622ff11157090
ash <uetani.0411@gmail.com>
2020-09-28 10:27:46 +0900

    likes

--- pointcard.rb
+++ pointcard.rb
@@ -1,19 +1,32 @@
 #自分の回答
-def unique_in_order(iterable)
-  if iterable.class == String
-    iterable.squeeze.chars
+def likes(names)
+  return "no one likes this" if names.empty?
+
+  if names.count == 1
+    "#{names[0]} likes this"
+  elsif names.count == 2
+    names.each { |n| }.join(" and ") << " like this"
+  elsif names.count == 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
   else
-    iterable.uniq
+　　　others_count = names.count -2
+    "#{names[0]}, #{names[1]} and #{others_count} others like this"   
   end
 end
 
 
 #理想の回答方法
-def unique_in_order(iterable)
-  case iterable
-    when String
-      iterable.gsub(/(.)\1*/, '\1').split('')
-    when Array
-      iterable.uniq
+def likes(names)
+  case names.size
+  when 0 
+    "no one likes this"
+  when 1 
+    "#{names[0]} likes this"
+  when 2
+    "#{names[0]} and #{names[1]} like this"
+  when 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
   end
 end
