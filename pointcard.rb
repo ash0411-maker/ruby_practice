f81c7916d7d8b2222febe871efee935adb7433eb
ash <uetani.0411@gmail.com>
2020-09-30 18:39:12 +0900

    IQ Test

--- pointcard.rb
+++ pointcard.rb
@@ -1,32 +1,23 @@
 #自分の回答
-def likes(names)
-  return "no one likes this" if names.empty?
+def iq_test(numbers)
+  numbers = numbers.split().map{|num| num.to_i}
+  even_count = numbers.select {|num| num.even?}.count
+  odd_count = numbers.select {|num| num.odd?}.count
 
-  if names.count == 1
-    "#{names[0]} likes this"
-  elsif names.count == 2
-    names.each { |n| }.join(" and ") << " like this"
-  elsif names.count == 3
-    "#{names[0]}, #{names[1]} and #{names[2]} like this"
-  else
-　　　others_count = names.count -2
-    "#{names[0]}, #{names[1]} and #{others_count} others like this"   
+  numbers.each_with_index do |num, i|
+    if even_count > odd_count
+      return i + 1 if num.odd?
+    else
+      return i + 1 if num.even?
+    end
   end
 end
 
 
-#理想の回答方法
-def likes(names)
-  case names.size
-  when 0 
-    "no one likes this"
-  when 1 
-    "#{names[0]} likes this"
-  when 2
-    "#{names[0]} and #{names[1]} like this"
-  when 3
-    "#{names[0]}, #{names[1]} and #{names[2]} like this"
-  else
-    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
-  end
+def iq_test(numbers)
+  nums = numbers.split.map(&:to_i).map(&:even?)
+  nums.count(true) > 1 ? nums.index(false) + 1 : nums.index(true) + 1
 end
+
+
+# https://qiita.com/ashketcham/items/d4c73bf52f666b4dd416
